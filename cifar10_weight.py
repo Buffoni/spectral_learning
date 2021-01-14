bff9c4ff8bcade938c291730ec615ebe1deeaca4
Enrico Civitelli <enrico.civitelli@gmail.com>
2021-01-14 17:53:57 +0100

    bugfix + dense version

--- cifar10_weight.py
+++ cifar10_weight.py
@@ -29,21 +29,21 @@ tf.random.set_seed(random_seed)
 # defining the training, validation, test sets
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 x_train, x_test = x_train.astype(np.float32), x_test.astype(np.float32)
-x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2,
-                                                  stratify=y_train, random_state=random_seed)
+# x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2,
+#                                                   stratify=y_train, random_state=random_seed)
 x_train = np.asarray([preprocess_input(resize(img, output_shape=[96, 96])) for img in tqdm(x_train)])
-x_val = np.asarray([preprocess_input(resize(img, output_shape=[96, 96])) for img in tqdm(x_val)])
+# x_val = np.asarray([preprocess_input(resize(img, output_shape=[96, 96])) for img in tqdm(x_val)])
 x_test = np.asarray([preprocess_input(resize(img, output_shape=[96, 96])) for img in tqdm(x_test)])
 
-def create_net(nclasses=10, spectral_out=1024, diag_reg=0.01, learning_rate=0.001):
+def create_net(nclasses=10, dense_out=512, reg=0.01, learning_rate=0.001):
   backbone = tf.keras.applications.MobileNetV2(input_shape=[96, 96, 3], include_top=False, weights='imagenet')
   backbone.trainable = False
   net = tf.keras.Sequential()
   net.add(backbone)
   net.add(tf.keras.layers.GlobalMaxPool2D())
-  net.add(Spectral(spectral_out, 
-                   activation="relu", 
-                   diag_regularizer=tf.keras.regularizers.l1(l1=diag_reg)))
+  net.add(tf.keras.layers.Dense(dense_out, 
+                                kernel_regularizer=tf.keras.regularizers.l1(l1=reg),
+                                activation="relu"))
   net.add(tf.keras.layers.Dense(nclasses, activation="softmax"))
   net.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
               loss="sparse_categorical_crossentropy",
@@ -53,30 +53,32 @@ def create_net(nclasses=10, spectral_out=1024, diag_reg=0.01, learning_rate=0.00
 
 nattempts = 3
 percentiles = list(range(0, 110, 10))
-diag_reg = [0, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1]
-hyperparameters = {"epochs": [15, 25],
-                   "lr": [1e-5, 1e-4, 1e-3]}
+diag_reg = [0, 1e-5, 5e-4, 1e-4, 1e-3, 1e-2, 1e-1]
+hyperparameters = {"epochs": [25, 50],
+                   "lr": [1e-4, 1e-3]}
 
 results = {"diag_reg": [], "percentile": [], "val_accuracy": []}
 for counter, dr in enumerate(diag_reg):
   print("{}-th training (of {}) with diagreg = {}".format(counter+1, len(diag_reg), dr))
   tic = time()
-  model = KerasClassifier(build_fn=lambda lr: create_net(learning_rate=lr, diag_reg=dr),
-                          epochs=10, batch_size=256, verbose=0)
-  model = GridSearchCV(estimator=model, param_grid=hyperparameters, n_jobs=1, cv=3, refit=False)
-  best_params = model.fit(x_train, y_train).best_params_
+  # model = KerasClassifier(build_fn=lambda lr: create_net(learning_rate=lr, reg=dr),
+  #                         epochs=10, batch_size=256, verbose=1)
+  # model = GridSearchCV(estimator=model, param_grid=hyperparameters, n_jobs=1, cv=3, refit=False)
+  # best_params = model.fit(x_train, y_train).best_params_
   print("Grid Search done in {:.3f} secs".format(time()-tic))
   for attempt in range(nattempts):
     tic = time()
-    model = create_net(learning_rate=best_params["lr"], diag_reg=dr)
-    model.fit(x_train, y_train, epochs=best_params["epochs"], batch_size=256, verbose=0)
+    model = create_net(learning_rate=0.1, reg=dr)
+    model.fit(x_train, y_train, epochs=1, batch_size=256, verbose=0)
     print("  {}-th training (of {}) done in {:.3f} secs".format(attempt+1, nattempts, time()-tic))
-    diag = model.layers[2].diag.numpy()
-    thresholds = [np.percentile(diag, q=perc) for perc in percentiles]
+    weights = model.layers[2].weights[0].numpy()
+    oshape = weights.shape
+    weights = weights.reshape(-1)
+    thresholds = [np.percentile(weights, q=perc) for perc in percentiles]
     for t, perc in tqdm(list(zip(thresholds, percentiles)), desc="  Removing the eigenvalues"):
-      diag[diag < t] = 0.0
-      model.layers[2].diag.assign(diag)
-      test_results = model.evaluate(x_val, y_val, batch_size=256, verbose=0)
+      weights[weights < t] = 0.0
+      model.layers[2].weights[0].assign(weights.reshape(oshape))
+      test_results = model.evaluate(x_test, y_test, batch_size=256, verbose=0)
       # storing the results
       results["diag_reg"].append("{}".format(dr))
       results["percentile"].append(perc)
