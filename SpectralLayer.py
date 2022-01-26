46dfaa125deb378ff797477fc9615cdcff5092a5
Lorenzo Giambagli <77098419+Jamba15@users.noreply.github.com>
2022-01-26 14:49:07 +0100

    Update: improved compatibility
    Restored compatibility with tf 2.7. (method 'core_ops' was no longer available)

--- SpectralLayer.py
+++ SpectralLayer.py
@@ -2,7 +2,7 @@ from tensorflow.python.keras.engine.base_layer import Layer
 from tensorflow.python.keras import activations, initializers, regularizers, constraints
 from tensorflow.python.util.tf_export import keras_export
 from tensorflow import multiply as mul
-from tensorflow.python.keras.layers.ops import core as core_ops
+from tensorflow import reduce_sum
 
 
 @keras_export('keras.layers.Spectral')
@@ -11,10 +11,12 @@ class Spectral(Layer):
                  units,
                  activation=None,
                  is_base_trainable=True,
-                 is_diag_trainable=True,
+                 is_diag_start_trainable=True,
+                 is_diag_end_trainable=True,
                  use_bias=False,
-                 base_initializer='GlorotUniform', # 'optimized_uniform',
-                 diag_initializer='optimized_uniform',
+                 base_initializer='GlorotUniform',
+                 diag_start_initializer='optimized_uniform',
+                 diag_end_initializer='optimized_uniform',
                  bias_initializer='zeros',
                  base_regularizer=None,
                  diag_regularizer=None,
@@ -24,6 +26,7 @@ class Spectral(Layer):
                  diag_constraint=None,
                  bias_constraint=None,
                  **kwargs):
+
         super(Spectral, self).__init__(
             activity_regularizer=activity_regularizer, **kwargs)
 
@@ -31,7 +34,8 @@ class Spectral(Layer):
         self.activation = activations.get(activation)
 
         self.is_base_trainable = is_base_trainable
-        self.is_diag_trainable = is_diag_trainable
+        self.is_diag_start_trainable = is_diag_start_trainable
+        self.is_diag_end_trainable = is_diag_end_trainable
         self.use_bias = use_bias
 
         # 'optimized_uniform' initializers optmized by Buffoni and Giambagli
@@ -39,17 +43,21 @@ class Spectral(Layer):
             self.base_initializer = initializers.RandomUniform(-0.02, 0.02)
         else:
             self.base_initializer = initializers.get(base_initializer)
-        if diag_initializer == 'optimized_uniform':
-            # self.diag_initializer = initializers.RandomUniform(-0.5, 0.5)
-            self.diag_initializer = initializers.RandomUniform(-0.5, 0.5)
-        else:
-            self.diag_initializer = initializers.get(diag_initializer)
-        self.bias_initializer = initializers.get(bias_initializer)
 
+        if diag_start_initializer == 'optimized_uniform':
+            self.diag_start_initializer = initializers.RandomUniform(-0.5, 0.5)
+        else:
+            self.diag_start_initializer = initializers.get(diag_start_initializer)
+
+        if diag_end_initializer == 'optimized_uniform':
+            self.diag_end_initializer = initializers.RandomUniform(-0.5, 0.5)
+        else:
+            self.diag_end_initializer = initializers.get(diag_end_initializer)
+
+        self.bias_initializer = initializers.get(bias_initializer)
         self.base_regularizer = regularizers.get(base_regularizer)
         self.diag_regularizer = regularizers.get(diag_regularizer)
         self.bias_regularizer = regularizers.get(bias_regularizer)
-
         self.base_constraint = constraints.get(base_constraint)
         self.diag_constraint = constraints.get(diag_constraint)
         self.bias_constraint = constraints.get(bias_constraint)
@@ -73,22 +81,22 @@ class Spectral(Layer):
         self.diag_end = self.add_weight(
             name='diag_end',
             shape=(1, self.units),
-            initializer=self.diag_initializer,
+            initializer=self.diag_end_initializer,
             regularizer=self.diag_regularizer,
             constraint=self.diag_constraint,
             dtype=self.dtype,
-            trainable=self.is_diag_trainable
+            trainable=self.is_diag_end_trainable
         )
 
         # \lambda_j
         self.diag_start = self.add_weight(
             name='diag_start',
             shape=(input_shape[-1], 1),
-            initializer=self.diag_initializer,
+            initializer=self.diag_start_initializer,
             regularizer=self.diag_regularizer,
             constraint=self.diag_constraint,
             dtype=self.dtype,
-            trainable=self.is_diag_trainable
+            trainable=self.is_diag_start_trainable
         )
 
         # bias
@@ -107,9 +115,48 @@ class Spectral(Layer):
         self.built = True
 
     def call(self, inputs, **kwargs):
-        return core_ops.dense(
-            inputs,
-            mul(self.base, self.diag_start - self.diag_end),
-            self.bias,
-            self.activation,
-            dtype=self._compute_dtype_object)
+      kernel = mul(self.base, self.diag_start - self.diag_end) 
+      outputs = tf.matmul(a=inputs, b=kernel)
+
+      if self.use_bias:
+        outputs = tf.nn.bias_add(outputs, self.bias)
+
+      if self.activation is not None:
+        outputs = self.activation(outputs)
+
+      return outputs
+
+    def direct_space(self):
+        return mul(self.base, self.diag_start - self.diag_end).numpy().T
+
+    def return_base(self):
+        c = self.base.shape[0]
+        N = reduce_sum(self.base.shape).numpy()
+        phi = np.eye(N)
+        phi[c:, :c] = self.base.numpy().T
+        return phi
+
+    def return_diag(self):
+        d = np.concatenate([self.diag_start.numpy()[:, 0], self.diag_end.numpy()[0, :]], axis=0)
+        return d
+
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({
+            'base_initializer': self.base_initializer,
+            'diag_start_initializer': self.diag_start_initializer,
+            'diag_end_initializer': self.diag_end_initializer,
+            'activation': self.activation,
+            'is_base_trainable': self.is_base_trainable,
+            'is_diag_start_trainable': self.is_diag_start_trainable,
+            'is_diag_end_trainable': self.is_diag_end_trainable,
+            'use_bias': self.use_bias,
+            'bias_initializer': self.bias_initializer,
+            'base_regularizer': self.base_regularizer,
+            'diag_regularizer': self.diag_regularizer,
+            'bias_regularizer': self.bias_regularizer,
+            'base_constraint': self.base_constraint,
+            'diag_constraint': self.diag_constraint,
+            'bias_constraint': self.bias_constraint,
+        })
+        return config
